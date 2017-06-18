a228ca3164b32e8e762f5e931fa63d56a31f8ed8
Butterknife <jurrewolters@gmail.com>
2017-06-18 08:09:55 +0200

    Add files via upload

--- Spacedships.py
+++ Spacedships.py
@@ -1,7 +1,6 @@
 #importing
 import pygame
 import random
-import time
 from pygame.locals import *
 
 #intializing
@@ -9,11 +8,13 @@ pygame.init()
 width, height = 850, 720
 screen = pygame.display.set_mode((width, height))
 keys = [False, False, False, False]
-health = 20
+health = 3
 shooting = 0
 badtimer = 100
 badtimer1 = 0
 badguys = [[640, 100]]
+badrects = [[]]
+clock = pygame.time.Clock()
 
 #importing pictures
 badshippic = pygame.image.load("resources/badship.png")
@@ -26,19 +27,16 @@ goodship = pygame.transform.scale(goodshippic,(64,64))
 bomb = pygame.transform.scale(bombpic,(64,64))
 
 #get rekt m8
-badrect = pygame.Rect(badship.get_rect())
+for badguy in badguys:
+    badrect = pygame.Rect(badship.get_rect())
 goodrect = pygame.Rect(goodship.get_rect())
 bombrect = pygame.Rect(bomb.get_rect())
 goodrect.x, goodrect.y = [425,650]
 bombrect.x, goodrect.y = [goodrect.x,goodrect.y]
 
-
 #the great loop
 while True:
-    screen.fill(0)
-    screen.blit(badship, (badrect.x, badrect.y))
-    screen.blit(goodship, (goodrect.x,goodrect.y))
-    pygame.display.flip()
+    clock.tick(60)
 
 #key events
     for event in pygame.event.get():
@@ -62,45 +60,48 @@ while True:
                 keys[2]=False
             elif event.key==pygame.K_d:
                 keys[3]=False
+
 #moving
     if keys[0]:
-        goodrect.y -= 1.5
+        goodrect.y -= 2.5
     elif keys[2]:
-        goodrect.y += 1.5
+        goodrect.y += 2.5
     if keys[1]:
-        goodrect.x -= 1.5
+        goodrect.x -= 2.5
     elif keys[3]:
-        goodrect.x += 1.5
+        goodrect.x += 2.5
 
-    badrect.y += 1
+    #badguyrect.y += 1
     bombrect.y -= 2
-#colliding party
-    if badrect.colliderect(goodrect):
-        health -= 1
 
-#badboys, badboys, what you gonna do?
+#badboys, badboys, what you gonna do?    
+    index = 0
     badtimer -= 1
     if badtimer == 0:
-        badguys.append([random.randint(64, 786),64])
-        badtimer = 100-(badtimer1*2)
-        if badtimer1 >= 35:
-            badtimer1 = 35
+        badguys.append([random.randint(64, 786), random.randint(-128,-64)])
+        badrect = pygame.Rect(badguys.get_rect())
+        badrects.append([badrect.x,badrect.y])
+        badtimer = 500-(badtimer1*2)
+        if badtimer1 >= 5:
+            badtimer1 = 5
         else:
-            badtimer1 += 5
+            badtimer1 += 1
 
-        index = 0
-        for badguy in badguys:
-            if badguy[0] < -64:
-                badguys.pop(index)
-            badguy[0] -= 7
-            index += 1
-        for badguy in badguys:
-            screen.blit(badship, badguy)
+    for badguy in badguys:
+        if badguy[0] < -64:
+            badguys.pop(index)
+        for rects in badrects:
+            badrect.y += 1
+        index += 1
+    for badguy in badguys:
+        screen.blit(badship, (badrect.x, badrect.y))
 
-    print(pygame.mouse.get_pos())
-            
-
-        
-        
-        
-    
+    #colliding party
+    for badguy in badguys:
+        if badrects.rect.colliderect(goodrect):
+            health -= 1
+            print("fck")
+    screen.blit(goodship, (goodrect.x,goodrect.y))
+    pygame.display.flip()
+    screen.fill(0)
+    #print(pygame.mouse.get_pos())
